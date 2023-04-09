7354cb5ca1269d81be1b2e889b30017248f9457d
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-04-09 14:18:00 -0600

    Add poros and spec

---
+++ app/poros/member.rb
@@ -0,0 +1,10 @@
+class Member
+  attr_reader :name, :role, :house, :patronus
+
+  def initialize(data)
+    @name = data[:name]
+    @role = data[:role]
+    @house = data[:house]
+    @patronus = data[:patronus]
+  end
+end
---
+++ spec/poros/member_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Member do
+  it "exists" do
+    attrs = {
+      name: "Chan (Fire Nation admiral)",
+      role: "Fire Nation Navy",
+      house: "Fire Nation Navy",
+      patronus: nil
+    }
+
+    member = Member.new(attrs)
+   
+    expect(member).to be_a(Member)
+    expect(member.name).to eq("Chan (Fire Nation admiral)")
+    expect(member.role).to eq("Fire Nation Navy")
+    expect(member.house).to eq("Fire Nation Navy")
+    expect(member.patronus).to eq(nil)
+  end
+end
